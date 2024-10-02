3d1c46af2d537cf0912d10ab97a643f1475a75cd
akankshaverm <priyaakanksha0507@gmail.com>
2024-10-02 21:50:31 +0530

    commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Route::prefix("user")->group(function () {});
+Route::prefix("admin")->group(function () {
+    Route::get("/",[AdminController::class,"dashboard"])->name("admin.dashboard");
+});
