9e945aed4f5c299935c8f4df62b9c209d5c5928b
akankshaverm <priyaakanksha0507@gmail.com>
2024-10-03 06:20:32 +0530

    rty

--- resources/views/admin/adminbase.blade.php
+++ resources/views/admin/adminbase.blade.php
@@ -14,32 +14,27 @@
             <div class="text-2xl font-bold text-pink-500">
                 <a href="#">AMBONS</a>
             </div>
-           
-           
-                <a href="#" class="bg-pink-500 text-white px-4 py-2 rounded-md">Logout</a>
-            
+
+
+            <a href="#" class="bg-pink-500 text-white px-4 py-2 rounded-md">Logout</a>
+
         </div>
     </header>
 
     <div class=" flex flex-1">
 
         <div class="w-64 h-screen bg-gray-900 shadow-lg hidden sm:block">
-            <div class="p-6">
-                <a href="#" class="text-white text-2xl font-semibold">Admin Sidebar</a>
+            <div class="p-6 ">
+                <a href="#" class="text-white text-2xl font-semibold"></a>
             </div>
             <nav class="mt-10">
-                <a href="#" class="block py-2.5 px-4 rounded transition duration-200 text-gray-200 hover:bg-gray-700 hover:text-white">
+                <a href="{{route('admin.dashboard')}}" class="block py-2.5 px-4 rounded transition duration-200 text-gray-200 hover:bg-gray-700 hover:text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-3-3v6m3 6v-6m-6-3h6m-6 6v-6" />
                     </svg>
                     Dashboard
                 </a>
-                <a href="#" class="block py-2.5 px-4 rounded transition duration-200 text-gray-200 hover:bg-gray-700 hover:text-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12h6v6H3v-6zm0 0l-1.293 1.293a1 1 0 011.414 1.414L5 9m0 0h12M5 9v12h12V9M5 9H2M15 9h6m0 0V4a1 1 0 00-1-1h-5a1 1 0 00-1 1v5" />
-                    </svg>
-                    Manage Categories
-                </a>
+                
                 <a href="#" class="block py-2.5 px-4 rounded transition duration-200 text-gray-200 hover:bg-gray-700 hover:text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4a2 2 0 012-2h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V4z" />
@@ -74,10 +69,9 @@
         </div>
 
         <div class=" w-10/12">
-            <div class="flex flex-1 p-5">
-                <h1>Hello Admin</h1>
-
-            </div>
+            @section('content')
+            
+            @show
 
         </div>
     </div>
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,29 @@
 @extends('admin.adminbase')
+
+@section('content')
+<div class="flex flex-1 p-5 flex-col gap-10">
+    <div class=" flex flex-1">
+        <h2 class=" border-l-4 border-pink-500 pl-2 text-xl">Admin Dashboard</h2>
+    </div>
+    <div class=" grid grid-cols-4 gap-5">
+        <div class="flex flex-col items-center justify-center bg-red-100 py-8 px-10">
+            <span class="mb-2 text-3xl font-extrabold">10+</span>
+            <span class="text-gray-500 dark:text-gray-400">Orders</span>
+        </div>
+        <div class="flex flex-col items-center justify-center bg-pink-100 py-8 px-10">
+            <span class="mb-2 text-3xl font-extrabold">10+</span>
+            <span class="text-gray-500 dark:text-gray-400">Orders</span>
+        </div>
+        <div class="flex flex-col items-center justify-center bg-green-100 py-8 px-10">
+            <span class="mb-2 text-3xl font-extrabold">10+</span>
+            <span class="text-gray-500 dark:text-gray-400">Orders</span>
+        </div>
+        <div class="flex flex-col items-center justify-center bg-orange-100 py-8 px-10">
+            <span class="mb-2 text-3xl font-extrabold">10+</span>
+            <span class="text-gray-500 dark:text-gray-400">Orders</span>
+        </div>
+
+    </div>
+
+</div>
+@endsection
